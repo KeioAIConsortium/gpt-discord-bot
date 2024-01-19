4e38b98f74a51a78f6ac2bb83bf6797e5d9e6556
sh1027 <shiori.ueda@keio.jp>
2024-01-19 05:22:27 +0000

    Add constants.py and main.py

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+DISCORD_CLIENT_ID = os.environ["DISCORD_CLIENT_ID"]
+
+# Send Messages, Create Public Threads, Send Messages in Threads, Manage Messages, Manage Threads, Read Message History, Use Slash Command
+BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_CLIENT_ID}&permissions=328565073920&scope=bot"
---
+++ src/main.py
@@ -0,0 +1,39 @@
+import logging
+from pathlib import Path
+
+import discord
+from discord.ext import commands
+from src.constants import BOT_INVITE_URL, DISCORD_BOT_TOKEN
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", level=logging.INFO
+)
+
+
+class GPTBot(commands.Bot):
+    def __init__(self, intents: discord.Intents) -> None:
+        super().__init__(command_prefix="/", intents=intents, help_command=None)
+
+    async def setup_hook(self):
+        # Enable cogs in discord_cogs directory (except for files starting with _)
+        cog_dir = Path(__file__).parent / "discord_cogs"
+        for cog_path in cog_dir.glob("*.py"):
+            cog_name = cog_path.stem
+            if cog_name.startswith("_"):
+                continue
+            await bot.load_extension(f"src.discord_cogs.{cog_name}")
+
+        await bot.tree.sync()
+
+    async def on_ready(self):
+        logging.info(f"We have logged in as {self.user}. Invite URL: {BOT_INVITE_URL}")
+
+
+if __name__ == "__main__":
+    # Define intents
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    # Create bot instance and run
+    bot = GPTBot(intents=intents)
+    bot.run(DISCORD_BOT_TOKEN)
