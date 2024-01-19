ca759e6e40b0c4a477c964cd5475ed96e5d850ca
sh1027 <shiori.ueda@keio.jp>
2024-01-19 12:40:19 +0000

    Add API response model

---
+++ src/models/api_responce.py
@@ -0,0 +1,22 @@
+import logging
+from dataclasses import dataclass
+from enum import Enum
+
+from openai import AsyncOpenAI
+from src.models.message import ContentImageFile, ContentText
+
+logger = logging.getLogger(__name__)
+
+client = AsyncOpenAI()
+
+
+class ResponceStatus(Enum):
+    OK = 0
+    ERROR = 1
+
+
+@dataclass
+class ResponceData:
+    status: ResponceStatus
+    content: list[ContentImageFile | ContentText] | None
+    status_text: str | None
