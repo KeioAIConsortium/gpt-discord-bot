ea68056810a53f37706f0ad51badde43a6d160fc
sh1027 <shiori.ueda@keio.jp>
2024-01-19 03:53:20 +0000

    Add OpenAI assistant API functions

---
+++ src/openai_api/assistants.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+
+from openai import AsyncOpenAI
+from src.base import Assistant
+
+logger = logging.getLogger(__name__)
+client = AsyncOpenAI()
+
+
+async def create_assistant(cfg: Assistant) -> Assistant:
+    responce = await client.beta.assistants.create(**cfg.input_to_api_create())
+    return Assistant.from_api_output(responce)
+
+
+async def list_assistants(limit: int = 20, order: str = "desc") -> list[Assistant]:
+    responce = await client.beta.assistants.list(limit=limit, order=order)
+    assistants = []
+    for d in responce.data:
+        assistants.append(Assistant.from_api_output(d))
+    return assistants
+
+
+async def get_assistant(id: str) -> Assistant:
+    responce = await client.beta.assistants.retrieve(id=id)
+    return Assistant.from_api_output(responce)
+
+
+async def update_assistant(cfg: Assistant) -> Assistant:
+    response = await client.beta.assistants.update(**cfg.input_to_api_update())
+    return Assistant.from_api_output(response)
+
+
+async def delete_assistant(id: str) -> None:
+    try:
+        responce = await client.beta.assistants.delete(id=id)
+        if responce.deleted:
+            logger.info(f"Deleted assistant {responce.id}")
+            return
+        else:
+            logger.info(f"Failed to delete assistant {responce.id}")
+            return
+    except ValueError as e:
+        logger.exception(e)
+
