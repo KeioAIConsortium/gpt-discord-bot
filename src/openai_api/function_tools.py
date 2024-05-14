51af791f1088edc3686daca99796cfe68e13e593
Jonah <jonahega@gmail.com>
2024-05-14 12:00:46 +0900

    Add get_wikipedia_page_content function to function_tools.py

--- src/openai_api/function_tools.py
+++ src/openai_api/function_tools.py
@@ -1,5 +1,8 @@
 from src.models.message import create_function
-from src.openai_api.functions import get_wikipedia_summary_function
+from src.openai_api.functions import (
+    get_wikipedia_summary_function,
+    get_wikipedia_page_content_function,
+)
 import json
 
 
@@ -21,11 +24,36 @@ def get_function_tool_outputs(tool_calls):
                     }
                 )
 
+        if tool.function.name == "get_wikipedia_page_content":
+            query = argumants_dict["query"]
+            content = get_wikipedia_page_content_function(query)
+
+            if content:
+                tool_outputs.append(
+                    {
+                        "tool_call_id": tool.id,
+                        "output": content,
+                    }
+                )
+
     return tool_outputs
+
 
 get_wikipedia_summary = create_function(
     name="get_wikipedia_summary",
     description="Search Wikipedia and retrieve a page summary and its URL",
+    parameters={
+        "query": {
+            "type": "string",
+            "description": "The search query to look up on Wikipedia",
+        },
+    },
+    required_parameters=["query"],
+)
+
+get_wikipedia_page_content = create_function(
+    name="get_wikipedia_page_content",
+    description="Retrieve the content of a Wikipedia page based on the search query and respond to the user with the relevant information",
     parameters={
         "query": {
             "type": "string",
@@ -42,4 +70,5 @@ def get_available_functions():
 
 available_functions = [
     get_wikipedia_summary,
+    get_wikipedia_page_content,
 ]
